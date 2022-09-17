5fe66691a39c30c328882aa415139ebd7dab1183
turtlelazy <ishraqmahid@gmail.com>
2022-09-17 17:20:28 -0400

    hw4.8

--- hw/E4_8.cpp
+++ hw/E4_8.cpp
@@ -4,3 +4,20 @@ Course : CSCI-135
 Instructor: Proffesor Genady Maryash
 Assignment: HW E4.8
 */
+
+/*
+Write a program that reads a word and prints each character of the word on a separate line. 
+For example, if the user provides the input "Harry", the program prints:   H   a   r   r   y
+*/
+
+#include <iostream>
+#include <string>
+int main(){
+    std::string s;
+
+    std::cin >> s;
+    for(int i = 0; i < s.length(); i++){
+        std::cout << s[i] << "\n";
+    }
+    return 0;
+}
