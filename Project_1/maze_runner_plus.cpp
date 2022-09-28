4c75015c9cd3c0ffcfb9f982b29b0ee414bd9400
turtlelazy <ishraqmahid@gmail.com>
2022-09-28 12:36:40 -0400

    project task c

--- Project_1/maze_runner_plus.cpp
+++ Project_1/maze_runner_plus.cpp
@@ -2,7 +2,7 @@
 Author: Ishraq Mahid
 Course : CSCI-135
 Instructor: Professor Genady Maryash
-Assignment: Project Task 2
+Assignment: Project Task 3
 */
 
 #include <iostream>
@@ -10,7 +10,7 @@ Assignment: Project Task 2
 
 int endY(int startY, char letter);
 int endX(int startX, char letter);
-void success(int goalX, int pX, int goalY, int pY);
+bool success(int goalX, int pX, int goalY, int pY);
 bool valid(int pX, int pY, int maze[6][6]);
 
 int maze[6][6] = {
@@ -19,10 +19,10 @@ int maze[6][6] = {
     1, 0, 1, 0, 0, 1,
     1, 0, 1, 0, 1, 1,
     1, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1
-    };
+    1, 1, 1, 1, 1, 1};
 
-int main(){
+int main()
+{
     int pX = 1;
     int pY = 0;
 
@@ -30,16 +30,23 @@ int main(){
     int goalY = 4;
     char s;
     while (std::cin >> s)
-    {                 // While the reading operation is a success
+    { // While the reading operation is a success
         //direction(s); // print the read word
-        int pX2 = endX(pX, s);
-        int pY2 = endY(pY, s);
 
-        if (valid(pX2,pY2,maze)){
-            pX = pX2;
-            pY = pY2;
+        if(s == ';'){
+            if(success(goalX, pX, goalY, pY)){
+                pX = 1;
+                pY = 0;
+            }
+
+            continue;
         }
 
+        if (valid(endX(pX, s), endY(pY, s), maze))
+        {
+            pX = endX(pX, s);
+            pY = endY(pY, s);
+        }
 
         // if(!valid(pX,pY,maze)){
         //     std::cout << "You are stuck in the maze.\n";
@@ -47,30 +54,25 @@ int main(){
         //     std::cout << pY << "Y\n";
         //     return 0;
         // }
-        
     }
     // std::cout << goalX << " " << pX << "\n";
     // std::cout << goalY << " " << pY << "\n";
 
-    success(goalX,pX,goalY,pY);
     return 0;
 }
 
-int endX(int startX, char letter){
-    if(letter != ' '){
+int endX(int startX, char letter)
+{
+    if (letter != ' ')
+    {
         if (letter == 'R')
         {
-            std::cout << letter;
-
             startX += 1;
         }
         else if (letter == 'L')
         {
-            std::cout << letter;
-
             startX -= 1;
         }
-
     }
     return startX;
 }
@@ -81,31 +83,34 @@ int endY(int startY, char letter)
     {
         if (letter == 'D')
         {
-            std::cout << letter;
-
             startY += 1;
         }
         else if (letter == 'U')
         {
-            std::cout << letter;
-
             startY -= 1;
         }
     }
     return startY;
 }
 
-void success(int goalX, int pX, int goalY, int pY){
-    if(goalX == pX && goalY == pY){
+bool success(int goalX, int pX, int goalY, int pY)
+{
+    if (goalX == pX && goalY == pY)
+    {
         std::cout << "You got out of the maze.\n";
+        return true;
     }
-    else{
+    else
+    {
         std::cout << "You are stuck in the maze.\n";
+        return false;
+
         // std::cout << pX << "X\n";
         // std::cout<< pY << "Y\n";
     }
 }
 
-bool valid(int pX, int pY, int maze[6][6]){
-    return maze[pY][pX]==0;
+bool valid(int pX, int pY, int maze[6][6])
+{
+    return maze[pY][pX] == 0;
 }
